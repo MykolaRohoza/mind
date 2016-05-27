cabb5011ac84a7c355fbbacb3fe5b15a093897b1
MykolaRohoza <rohoza_mykola@rambler.ru>
2016-05-27 09:26:14 +0300

    1

--- V/view_users.php
+++ V/view_users.php
@@ -4,15 +4,15 @@
     <div class="row">
         <div class="col-sm-8">
             <?php if($articles):?> 
-            <?php foreach ($articles as $article):?>
-                <div class="article article-<?=$article['article_img_place']?> clearfix">
-                    <h3><?=$article['article_title']?></h3>
-                    <img src="<?='http://' . $_SERVER['SERVER_NAME'] . '/images/carousel/' . $article['article_img_name']?>" alt="<?=$article['image_alt']?>">
+            <?php foreach ($users as $user):?>
+                <div class="article article-left clearfix">
+                    <h3><?=$user['article_title']?></h3>
+                    <img src="<?='http://' . $_SERVER['SERVER_NAME'] . '/images/carousel/' . $user['article_img_name']?>" alt="<?=$user['image_alt']?>">
                         <?php if($isAdmin):?>
-                            <a href="/edit/<?=$article['id_article']?>" class=" edit"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span></a>
+                            <a href="/edit/<?=$user['id_article']?>" class=" edit"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span></a>
                         <?php endif;?>
 
-                <?=$article['article_text']?>
+                <?=$user['article_text']?>
                 </div>
             <?php endforeach;?>
             <?php endif;?>       
